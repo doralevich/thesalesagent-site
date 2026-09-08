62e83cb870a8831b5b8a939c11a6d4a7cf530f55
Donna Paulsen <daveo@designsbydaveo.com>
2026-09-08 10:51:28 -0400

    Add Google Search Console verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="PfeLDH0WtWo5YNq_oGDzrv1Fl5y4DNwWKFCl68xJlvI" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
